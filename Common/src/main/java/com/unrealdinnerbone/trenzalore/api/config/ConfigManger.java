a895e30d3393550ea1642b374797b50eb7e416e8
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2023-06-06 18:56:23 -0500

    Make Simple ConfigManger

---
+++ Common/src/main/java/com/unrealdinnerbone/trenzalore/api/config/ConfigManger.java
@@ -0,0 +1,37 @@
+package com.unrealdinnerbone.trenzalore.api.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mojang.logging.LogUtils;
+import com.unrealdinnerbone.trenzalore.api.platform.Services;
+import org.slf4j.Logger;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Supplier;
+
+public class ConfigManger {
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    public static <T> T getOrCreateConfig(String name, Class<T> tClass, Supplier<T> defaultValue) {
+        Path configPath = Services.PLATFORM.getConfigPath();
+        Path config = configPath.resolve(name + ".json");
+        if(!Files.exists(config)) {
+            T defaultConfig = defaultValue.get();
+            try {
+                Files.write(config, GSON.toJson(defaultConfig).getBytes());
+            } catch (Exception e) {
+                LOGGER.error("Error saving config. Defaulting to default config", e);
+            }
+            return defaultConfig;
+        }
+        try {
+            return GSON.fromJson(Files.readString(config), tClass);
+        } catch (Exception e) {
+            LOGGER.error("Error loading config. Defaulting to default config", e);
+            return defaultValue.get();
+        }
+    }
+
+}
