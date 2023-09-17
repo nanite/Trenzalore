bfbd1c0dcb778100a3f860b27a00c8a8a585ad34
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-09-17 12:26:49 -0500

    Fix Crashes

--- common/src/main/java/com/unrealdinnerbone/trenzalore/Trenzalore.java
+++ common/src/main/java/com/unrealdinnerbone/trenzalore/Trenzalore.java
@@ -2,15 +2,14 @@ package com.unrealdinnerbone.trenzalore;
 
 import com.unrealdinnerbone.trenzalore.api.platform.Services;
 import com.unrealdinnerbone.trenzalore.api.platform.services.IRegistry;
-import com.mojang.logging.LogUtils;
 import com.unrealdinnerbone.trenzalore.api.registry.RegistryObjects;
-import org.slf4j.Logger;
 
 import java.util.List;
 import java.util.ServiceLoader;
 
 public class Trenzalore {
     public static final String MOD_ID = "trenzalore";
+
     public static void init() {
         ServiceLoader.load(IRegistry.class).forEach(iRegistry -> {
             List<RegistryObjects<?>> registryObjects = iRegistry.getRegistryObjects();
@@ -18,4 +17,5 @@ public class Trenzalore {
             iRegistry.afterRegistered();
         });
     }
+
 }
